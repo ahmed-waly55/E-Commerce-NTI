6fdc0c17470b7959e9e2b45230008efc1b28a08c
ahmed waly <ahmedwaly074@gmail.com>
2024-12-20 14:55:05 +0200

    fix subcategory method create fix import

--- src/subcategories/subcategories.validation.ts
+++ src/subcategories/subcategories.validation.ts
@@ -1,26 +1,28 @@
-import { body, param } from "express-validator";
-import categoriesSchema from "./subcategories.schema";
+import {body, param} from "express-validator";
 import validatorMiddleware from "../middlewares/validator.middleware";
+import categoriesSchema from "../categories/categories.schema";
+
 
 class subCategoriesValidation{
-    createOne = [body('name')
+
+    createOne = [
+        body('name')
         .notEmpty()
         .withMessage('subcategory name is required')
         .isLength({min:2,max:50})
         .withMessage('invaled length'),
         body('category').notEmpty().withMessage('category  is required')
         .isMongoId().withMessage('invalid id')
-        .custom(async(val:string)=>{
+        .custom(async (val: string, {req}) => {
             const category = await categoriesSchema.findById(val);
-            if(!category) throw new Error('category not found');
+            if (!category) throw new Error(`${req.__('validation_value')}`);
             return true;
         }),
         validatorMiddleware]
 
     updateOne = [
         param('id').isMongoId().withMessage('invalid id'),
-        body('name')
-        .optional()
+        body('name').optional()
         .isLength({min:2,max:50})
         .withMessage('invaled length'),
         body('category').optional()
